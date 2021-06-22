1f2bd8af2a3c9f998dfa90210c1f2c2b4fb5a90e
Utsav Apurva <76875307+speeeedy-899@users.noreply.github.com>
2021-06-22 20:36:58 +0530

    Update app.js for the preloader

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-/* -----------------------------------------------
-/* How to use? : Check the GitHub README
-/* ----------------------------------------------- */
 
-/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
-/*
-particlesJS.load('particles-js', 'particles.json', function() {
-  console.log('particles.js loaded - callback');
+window.addEventListener("load", function(){
+  const preload = document.querySelector(".preload");
+  preload.classList.add("preload-finish");
 });
-*/
+
+
 
 /* Otherwise just put the config content (json): */
 
